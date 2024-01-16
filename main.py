ce660775bd38de87aa19b325cf8a06595fff1492
Isil Oral <isiloral.io@gmail.com>
2024-01-16 13:58:02 +0000

    Coded solution to string slicingexercise

---
+++ main.py
@@ -0,0 +1,21 @@
+#Exercise: Indexing and slicing
+
+url = 'https://github.com/codedthinking/tender-home-bias/releases/download/v1.0/ted-sample.csv'
+
+#Use indexing, slicing to fill in the following output.
+#Copy/paste is not allowed
+
+file_name = url[start_index:end_index]
+#use len() function: len('ted-sample.csv'). This will give the length of the searched string.
+print(file_name) #'ted-sample.csv'
+
+
+Protocol =
+print(protcol) #'https'
+
+host_name = 
+#again by using len(), you can find it. 
+print(host_name) #'github.com'
+
+#Use string composition to construct https://github.com/ted-sample.csv
+output = 
